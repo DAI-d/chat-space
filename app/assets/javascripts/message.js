3bd9f0370ca2d8383eb4b73b87fce9ab2b727dcb
DAI-d <mkaeeidjai@yahoo.co.jp>
2019-09-27 01:17:59 +0900

    update-auto_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,11 +2,11 @@ $(function(){
   function buildHTML(message){
     var content = message.content ? `${ message.content }` : "";
     var image = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                     ${message.user_name}
-                    </div>                    
+                    </div>
                     <div class="upper-message__date">
                     ${message.date}
                     </div>
@@ -25,7 +25,7 @@ $(function(){
     var position = target.offset().top + $('.messages').scrollTop();
     $('.messages').animate({
       scrollTop: position
-    }, 100, 'swing');
+    }, 50, 'swing');
   }
 
   $('#new_message').on('submit', function(e){
@@ -51,5 +51,29 @@ $(function(){
       alert('メッセージを入力してください！');
     })
     return false;
-  })
-})
+  });
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: './api/messages',
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      messages.forEach(function(message){
+      insertHTML = buildHTML(message);
+      $('.messages').append(insertHTML);
+    })
+    scrollBottom();
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+});
